57eb333e9dbe781fc76859828a680ebd7eacf2c1
MarcusviniciusLsantos <marcusvinici25@gmail.com>
2019-12-20 02:20:45 -0300

    app full

---
+++ front-app/src/App.js
@@ -0,0 +1,127 @@
+import React, { Component } from 'react';
+import './App.css';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: false,
+      formData: {
+        salary: 4500,
+        age: 25,
+        loan: 10000
+      },
+      result: ""
+    };
+  }
+
+  handleChange = (event) => {
+    const value = event.target.value;
+    const name = event.target.name;
+    var formData = this.state.formData;
+    formData[name] = value;
+    this.setState({
+      formData
+    });
+  }
+
+  handlePredictClick = (event) => {
+    const formData = this.state.formData;
+    this.setState({ isLoading: true });
+    fetch('http://localhost:5000/prediction/', 
+      {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify(formData)
+      })
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          result: response.result,
+          isLoading: false
+        });
+      });
+  }
+
+  handleCancelClick = (event) => {
+    this.setState({ result: "" });
+  }
+
+  render() {
+    const isLoading = this.state.isLoading;
+    const formData = this.state.formData;
+    const result = this.state.result;
+
+    return (
+      <Container>
+        <div>
+          <h1 className="title">Data Credit Classifier</h1>
+        </div>
+        <div className="content">
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Label>Salary</Form.Label>
+                <Form.Control placeholder="Ex: 4300" value={formData.salary} onChange={this.handleChange} name="salary"/>
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Age</Form.Label>
+                <Form.Control
+                  placeholder="Ex: 4300" 
+                  value={formData.age}
+                  name="age"
+                  onChange={this.handleChange}/> 
+              </Form.Group>
+            </Form.Row>
+            <Form.Row>
+              <Form.Group as={Col}>
+                <Form.Label>Loan</Form.Label>
+                <Form.Control placeholder="Ex: 4300" value={formData.loan} onChange={this.handleChange} name="loan"/>
+              </Form.Group>
+            </Form.Row>
+            <Row>
+              <Col>
+                <Button
+                  block
+                  variant="success"
+                  disabled={isLoading}
+                  onClick={!isLoading ? this.handlePredictClick : null}>
+                  { isLoading ? 'Making prediction' : 'Predict' }
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  block
+                  variant="danger"
+                  disabled={isLoading}
+                  onClick={this.handleCancelClick}>
+                  Reset prediction
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+          {result === "" ? null :
+            (<Row>
+              <Col className="result-container">
+                <h5 id="result">{result}</h5>
+              </Col>
+            </Row>)
+          }
+        </div>
+      </Container>
+    );
+  }
+}
+
+export default App;
